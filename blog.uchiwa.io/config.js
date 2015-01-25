04b1e58284a9e26435ae891dc8dc494a3a164a14
Simon Plourde <splourde@lapresse.ca>
2015-01-25 18:21:22 -0500

    Add production config

--- blog.uchiwa.io/config.js
+++ blog.uchiwa.io/config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://blog.uchiwa.io',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -22,7 +22,7 @@ config = {
 
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         }
